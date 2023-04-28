ab59bcbe417fcdda1c852cfec5cbd57e24a2d062
kaspi174 <kczajka174@gmail.com>
2023-04-28 11:03:57 +0100

    update to path

--- Web Page/scripts/loginform.js
+++ Web Page/scripts/loginform.js
@@ -3,6 +3,7 @@ function login() {
   var password = document.getElementById("password").value;
   if (username === "myusername" && password === "mypassword") {
     alert("Login successful!");
+    window.location.href = "/Web Page/directory/index.html";
   } else {
     alert("Login failed.");
   }
