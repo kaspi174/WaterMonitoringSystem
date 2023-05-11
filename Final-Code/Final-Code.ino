0afb56144328d600e6062df4578f17be87079a0f
kaspi174 <kczajka174@gmail.com>
2023-05-11 10:48:18 +0100

    SUMMARY

--- Final-Code/Final-Code.ino
+++ Final-Code/Final-Code.ino
@@ -1,41 +1,48 @@
-//Kacper Czajka
-//B00123855
-//Technological University Dublin
-//Water Monitoring System
-
 #include <EEPROM.h>
 #include "GravityTDS.h"
 #include <SoftwareSerial.h>
-
-
+#include <ArduinoJson.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 GravityTDS gravityTds;
 
-float temperature = 19.9,tdsValue = 0;
- 
-#define rxPin 10               //pin 10 as tx
-#define txPin 11               //pin 11 as rx
-#define TdsSensorPin A1        //TDS pin 
-#define SensorPin A2           //pH meter pin
-#define Offset 0.82            //deviation compensated
+float temperature = 25, tdsValue = 0;
+
+
+#define ONE_WIRE_BUS 4
+#define rxPin 10
+#define txPin 11 
+#define TdsSensorPin A1
+#define SensorPin A2            //pH meter Analog output to Arduino Analog Input 1
+#define Offset 0.00            //deviation compensate
 #define LED 13
 #define samplingInterval 20
 #define printInterval 800
-#define ArrayLenth  40         //times of collection
-
-// set up a new serial port
-SoftwareSerial Sigfox =  SoftwareSerial(rxPin, txPin);
-
+#define ArrayLenth  40    //times of collection
+#define MESSAGE_SIZE 5
 #define DEBUG 1
 
-//Message buffer
-uint8_t msg[12];
+SoftwareSerial Sigfox(rxPin, txPin);
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
 
 int pHArray[ArrayLenth];   //Store the average value of the sensor feedback
 int pHArrayIndex=0;
 
+
+
 void setup() {
+
+  Sigfox.begin(9600);
   Serial.begin(9600);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(rxPin, INPUT);
+  pinMode(txPin, OUTPUT);
+  
+  delay(100);
+  sensors.begin();
 
    gravityTds.setPin(TdsSensorPin);
    gravityTds.setAref(5.0);  //reference voltage on ADC, default 5.0V on Arduino UNO
@@ -43,25 +50,38 @@ void setup() {
    gravityTds.begin();  //initialization
 
    pinMode(LED,OUTPUT);
+
+
+  String id = getID();
+  if(DEBUG){
+    Serial.print("Sigfox Device ID: ");
+    Serial.println(id);
+  }
 }
-void loop() {
+void loop(){
+
+  sensors.requestTemperatures(); 
+  float temp = sensors.getTempC(0);
   int sensorValue = analogRead(A0);
   float volt = sensorValue * (5.0 / 1024.0);
   float ntu = -1120.4*square(volt)+5742.3*volt-4353.8; 
 
-  gravityTds.setTemperature(temperature);     // set the temperature and execute temperature compensation
-  gravityTds.update();                        //sample and calculate
-  tdsValue = (gravityTds.getTdsValue())-32;   // then get the value
+  gravityTds.setTemperature(temperature);  // set the temperature and execute temperature compensation
+  gravityTds.update();  //sample and calculate
+  tdsValue = gravityTds.getTdsValue();  // then get the value
 
   static unsigned long samplingTime = millis();
   static unsigned long printTime = millis();
   static float pHValue,voltage;
+
+  
+
   if(millis()-samplingTime > samplingInterval)
   {
       pHArray[pHArrayIndex++]=analogRead(SensorPin);
       if(pHArrayIndex==ArrayLenth)pHArrayIndex=0;
       voltage = avergearray(pHArray, ArrayLenth)*5.0/1024;
-      pHValue = 3.6*voltage+Offset;
+      pHValue = 3.5*voltage+Offset;
       samplingTime=millis();
   }
   if(millis() - printTime > printInterval)   //Every 800 milliseconds, print a numerical, convert the state of the LED indicator
@@ -75,7 +95,7 @@ void loop() {
         printTime=millis();
       Serial.println("-----");
   }
-
+  Serial.println("");
   Serial.println("-----");
   Serial.println("Turbidity:");
   Serial.print("V");
@@ -84,12 +104,33 @@ void loop() {
   Serial.println(ntu);
   Serial.println("-----");
 
-  Serial.println("-----");
+  
   Serial.println("TDS:");
   Serial.print(tdsValue,0);
   Serial.println("ppm");
   Serial.println("-----");
-  delay(3000);
+  Serial.print("Celsius temperature: ");
+  Serial.print(sensors.getTempCByIndex(0)); 
+  Serial.print(" - Fahrenheit temperature: ");
+  Serial.println(sensors.getTempFByIndex(0));
+  Serial.println("-----");
+
+
+  uint8_t msg[MESSAGE_SIZE];
+  msg[0] = (uint8_t)(pHValue + 128);   // convert pH value to 1-byte hex value
+  msg[1] = (uint8_t)(volt * 51);       // convert voltage to 1-byte hex value (multiply by 51 instead of 255 to avoid overflow)
+  msg[2] = (uint8_t)(ntu + 128);       // convert ntu to 1-byte hex value
+  msg[3] = (uint8_t)(tdsValue / 100);  // convert tdsValue to 1-byte hex value (divide by 100 to fit within 1 byte)
+  msg[4] = (uint8_t)(temp);            // convert temperature value to 1-byte hex value
+
+
+  
+
+  // send the message using the sendMessage function
+  sendMessage(msg, MESSAGE_SIZE);
+  Serial.write(msg, MESSAGE_SIZE);
+  // wait for 10 minutes (600000 milliseconds)
+  delay(30000);
   
 }
 double avergearray(int* arr, int number){
@@ -101,7 +142,7 @@ double avergearray(int* arr, int number){
     Serial.println("Error number for the array to avraging!/n");
     return 0;
   }
-  if(number<5){                         //less than 5, calculated directly statistics
+  if(number<5){   //less than 5, calculated directly statistics
     for(i=0;i<number;i++){
       amount+=arr[i];
     }
@@ -116,7 +157,7 @@ double avergearray(int* arr, int number){
     }
     for(i=2;i<number;i++){
       if(arr[i]<min){
-        amount+=min;       //arr<min
+        amount+=min;        //arr<min
         min=arr[i];
       }else {
         if(arr[i]>max){
@@ -125,58 +166,12 @@ double avergearray(int* arr, int number){
         }else{
           amount+=arr[i]; //min<=arr<=max
         }
-      }
-    }
+      }//if
+    }//for
     avg = (double)amount/(number-2);
-  }
+  }//if
   return avg;
 }
-
-
-// the setup function runs once when you press reset or power the board
-void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-
-  if(DEBUG){
-    Serial.begin(9600);
-  }
-
-  // open Wisol communication
-   // define pin modes for tx, rx:
-  pinMode(rxPin, INPUT);
-  pinMode(txPin, OUTPUT);
-  Sigfox.begin(9600);
-  delay(100);
-  getID();
-  delay(100);
-  getPAC();
-}
-
-// the loop function runs over and over again forever
-void loop() {
-  msg[0]=0xC0;
-  msg[1]=0xFF;
-  msg[2]=0xEE;
-
-  sendMessage(msg, 3);
-
-  // In the ETSI zone, due to the reglementation, an object cannot emit more than 1% of the time hourly
-  // So, 1 hour = 3600 sec
-  // 1% of 3600 sec = 36 sec
-  // A Sigfox message takes 6 seconds to emit
-  // 36 sec / 6 sec = 6 messages per hours -> 1 every 10 minutes
-  delay(60000);
-}
-
-void blink(){
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);    
-}
-
-//Get Sigfox ID
 String getID(){
   String id = "";
   char output;
@@ -192,55 +187,28 @@ String getID(){
     delay(10);
   }
 
-  if(DEBUG){
-    Serial.println("Sigfox Device ID: ");
-    Serial.println(id);
-  }
-
   return id;
 }
-//Get PAC number
-String getPAC(){
-  String pac = "";
-  char output;
 
-  Sigfox.print("AT$I=11\r");
-  while (!Sigfox.available()){
-     blink();
-  }
-
-  while(Sigfox.available()){
-    output = Sigfox.read();
-    pac += output;
-    delay(10);
-  }
-
-  if(DEBUG){
-    Serial.println("PAC number: ");
-    Serial.println(pac);
-  }
-
-  return pac;
-}
-
-
-//Send Sigfox Message
+// send a Sigfox message
 void sendMessage(uint8_t msg[], int size)
 {
-  Serial.println("Inside sendMessage");
+  if(DEBUG){
+    Serial.println("Sending message...");
+  }
 
   String status = "";
   String hexChar = "";
   String sigfoxCommand = "";
   char output;
 
+  // build the AT command with the message data
   sigfoxCommand += "AT$SF=";
-
   for (int i = 0; i < size; i++)
   {
     hexChar = String(msg[i], HEX);
 
-    //padding
+    // padding for single-digit hex values
     if (hexChar.length() == 1)
     {
       hexChar = "0" + hexChar;
@@ -249,16 +217,16 @@ void sendMessage(uint8_t msg[], int size)
     sigfoxCommand += hexChar;
   }
 
-  Serial.println("Sending...");
-  Serial.println(sigfoxCommand);
+  // send the command to the Sigfox module
   Sigfox.println(sigfoxCommand);
 
+  // wait for the response from the module
   while (!Sigfox.available())
   {
-    Serial.println("Waiting for response");
-    delay(1000);
+    blink();
   }
 
+  // read the response
   while (Sigfox.available())
   {
     output = (char)Sigfox.read();
@@ -266,9 +234,15 @@ void sendMessage(uint8_t msg[], int size)
     delay(10);
   }
 
-  Serial.println();
-  Serial.print("Status \t");
-  Serial.println(status);
+  if(DEBUG){
+    Serial.print("Status: ");
+    Serial.println(status);
+  }
 }
-
-
+void blink(){
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off
+  delay(1000);    
+}
+ 
